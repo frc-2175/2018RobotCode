847e90410bc85e81a6220d37cd07ba2d3f1bf2d3
Nupur Kumar <kumarnupurk@gmail.com>
2018-01-10 20:45:37 -0600

    Begin intake

---
+++ src/org/usfirst/frc/team2175/subsystem/IntakeSubsystem.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team2175.subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class IntakeSubsystem extends Base_S{
+	private WPI_TalonSRX rollerBar;
+public IntakeSubsystem () {
+	rollerBar = new WPI_TalonSRX(6);
+	
+	}
+public void runRollerBarIn () {
+	rollerBar.set(0.7);
+}
+public void runRollerBarOut () {
+	rollerBar.set(-0.5);
+					} 
+}
