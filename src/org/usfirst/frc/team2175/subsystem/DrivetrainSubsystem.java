b858fbee2587d28f08cddc252aff356e9c9b29e1
Adisesh <adisesh.viruru@gmail.com>
2018-01-10 18:03:04 -0600

    Begin Working on Drivetrain subsystem

---
+++ src/org/usfirst/frc/team2175/subsystem/DrivetrainSubsystem.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team2175.subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class DrivetrainSubsystem {
+	private WPI_TalonSRX leftMaster;
+	private WPI_TalonSRX leftSlaveOne;
+	private WPI_TalonSRX leftSlaveTwo;
+	private WPI_TalonSRX rightMaster;
+	private WPI_TalonSRX rightSlaveOne;
+	private WPI_TalonSRX rightSlaveTwo;
+	private DifferentialDrive robotDrive;
+
+	public DrivetrainSubsystem() {
+		leftMaster = new WPI_TalonSRX(0);
+		leftSlaveOne = new WPI_TalonSRX(1);
+		leftSlaveTwo = new WPI_TalonSRX(2);
+		rightMaster = new WPI_TalonSRX(3);
+		rightSlaveOne = new WPI_TalonSRX(4);
+		rightSlaveTwo = new WPI_TalonSRX(5);
+		leftSlaveOne.follow(leftMaster);
+		leftSlaveTwo.follow(leftMaster);
+		rightSlaveOne.follow(rightMaster);
+		rightSlaveTwo.follow(rightMaster);
+		robotDrive = new DifferentialDrive(leftMaster, rightMaster);
+
+	}
+
+	public void robotDrive(double xSpeed, double zRotation) {
+		robotDrive.arcadeDrive(xSpeed, zRotation, false);
+	}
+}
