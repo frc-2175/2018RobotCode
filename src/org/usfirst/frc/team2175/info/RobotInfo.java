915201831accbee028b9253f2c734a03d5897f46
Erik Nelson <eriknelson@mnmsa.org>
2018-01-12 19:26:33 -0600

    Change Talon initialization to RobotInfo

--- src/org/usfirst/frc/team2175/info/RobotInfo.java
+++ src/org/usfirst/frc/team2175/info/RobotInfo.java
@@ -12,6 +12,9 @@ public class RobotInfo {
 	public static final String LEFT_MOTOR_MASTER = "drivetrain.motor.left.master";
 	public static final String LEFT_MOTOR_SLAVE1 = "drivetrain.motor.left.slave1";
 	public static final String LEFT_MOTOR_SLAVE2 = "drivetrain.motor.left.slave2";
+	public static final String RIGHT_MOTOR_MASTER = "drivetrain.motor.right.master";
+	public static final String RIGHT_MOTOR_SLAVE1 = "drivetrain.motor.right.slave1";
+	public static final String RIGHT_MOTOR_SLAVE2 = "drivetrain.motor.right.slave2";
 	public static final String INTAKE_MOTOR = "intake.motor";
 	private HashMap<String, Object> info;
 	private final boolean isComp;
@@ -29,8 +32,11 @@ public class RobotInfo {
 	
 	private void populate() {
 		put(LEFT_MOTOR_MASTER, new WPI_TalonSRX(0), new WPI_TalonSRX(1));
-		put(LEFT_MOTOR_MASTER, new WPI_TalonSRX(5), new WPI_TalonSRX(3));
-		put(LEFT_MOTOR_MASTER, new WPI_TalonSRX(2), new WPI_TalonSRX(4));
+		put(LEFT_MOTOR_SLAVE1, new WPI_TalonSRX(5), new WPI_TalonSRX(3));
+		put(LEFT_MOTOR_SLAVE2, new WPI_TalonSRX(2), new WPI_TalonSRX(4));
+		put(RIGHT_MOTOR_MASTER, new WPI_TalonSRX(4), new WPI_TalonSRX(6));
+		put(RIGHT_MOTOR_SLAVE1, new WPI_TalonSRX(7), new WPI_TalonSRX(8));
+		put(RIGHT_MOTOR_SLAVE2, new WPI_TalonSRX(9), new WPI_TalonSRX(3));
 		put(INTAKE_MOTOR, new WPI_TalonSRX(1), new WPI_TalonSRX(3));
 	}
 	
--- src/org/usfirst/frc/team2175/subsystem/DrivetrainSubsystem.java
+++ src/org/usfirst/frc/team2175/subsystem/DrivetrainSubsystem.java
@@ -1,10 +1,14 @@
 package org.usfirst.frc.team2175.subsystem;
 
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.info.RobotInfo;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class DrivetrainSubsystem extends Base_S{
+	private RobotInfo robotInfo;
 	private WPI_TalonSRX leftMaster;
 	private WPI_TalonSRX leftSlaveOne;
 	private WPI_TalonSRX leftSlaveTwo;
@@ -14,12 +18,13 @@ public class DrivetrainSubsystem extends Base_S{
 	private DifferentialDrive robotDrive;
 
 	public DrivetrainSubsystem() {
-		leftMaster = new WPI_TalonSRX(0);
-		leftSlaveOne = new WPI_TalonSRX(1);
-		leftSlaveTwo = new WPI_TalonSRX(2);
-		rightMaster = new WPI_TalonSRX(3);
-		rightSlaveOne = new WPI_TalonSRX(4);
-		rightSlaveTwo = new WPI_TalonSRX(5);
+		robotInfo = ServiceLocator.get(RobotInfo.class);
+		leftMaster = robotInfo.get(RobotInfo.LEFT_MOTOR_MASTER);
+		leftSlaveOne = robotInfo.get(RobotInfo.LEFT_MOTOR_SLAVE1);
+		leftSlaveTwo = robotInfo.get(RobotInfo.LEFT_MOTOR_SLAVE2);
+		rightMaster = robotInfo.get(RobotInfo.RIGHT_MOTOR_MASTER);
+		rightSlaveOne = robotInfo.get(RobotInfo.RIGHT_MOTOR_SLAVE1);
+		rightSlaveTwo = robotInfo.get(RobotInfo.RIGHT_MOTOR_SLAVE2);
 		leftSlaveOne.follow(leftMaster);
 		leftSlaveTwo.follow(leftMaster);
 		rightSlaveOne.follow(rightMaster);
