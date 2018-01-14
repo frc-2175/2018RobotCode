f35e428d12fb911e90881276c5b0cb9ec18a8295
NoahConnors <noahconnors@mnmsa.org>
2018-01-13 19:41:49 -0600

    Change DriverStation casing

---
+++ src/org/usfirst/frc/team2175/control/DriverStation.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team2175.control;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.info.RobotInfo;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DriverStation {
+	private Joystick leftJoystick;
+	private Joystick rightJoystick;
+	private Joystick gamepad;
+	RobotInfo robotInfo;
+	
+	public DriverStation() {
+		ServiceLocator.register(this);
+		leftJoystick = robotInfo.get(RobotInfo.LEFT_JOYSTICK);
+		rightJoystick = robotInfo.get(RobotInfo.RIGHT_JOYSTICK);
+		gamepad = robotInfo.get(RobotInfo.GAMEPAD);
+	}
+	
+	public double getMoveValue() {
+		return leftJoystick.getY();
+	}
+	
+	public double getTurnValue() {
+		return rightJoystick.getX();
+	}
+}
