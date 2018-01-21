8299c0e7edf137a00279da89b72c0723aed6d7db
Kevin La Casse <kl.forthwind@gmail.com>
2018-01-21 14:34:29 -0600

    Update Service Locator

--- src/org/usfirst/frc/team2175/ServiceLocator.java
+++ src/org/usfirst/frc/team2175/ServiceLocator.java
@@ -4,33 +4,39 @@ import java.util.HashMap;
 import java.util.logging.Logger;
 
 public class ServiceLocator {
-    private static final Logger log =
-            Logger.getLogger(ServiceLocator.class.getName());
+	private static final Logger log = Logger.getLogger(ServiceLocator.class.getName());
 
-    private static final HashMap<Class<?>, Object> MAP = new HashMap<>();
+	private static final HashMap<Class<?>, Object> MAP = new HashMap<>();
 
-    public static void clear() {
-        MAP.clear();
-    }
+	public static void clear() {
+		MAP.clear();
+	}
 
-    public static void register(final Object instance) {
-        log.info("Registering '" + instance.getClass().getName() + "'");
+	public static void register(final Object instance) {
+		log.info("Registering '" + instance.getClass().getName() + "'");
 
-        final Object previousObject = MAP.put(instance.getClass(), instance);
-        if (previousObject != null) {
-            throw new IllegalStateException(
-                    "MAP already contains value for key=" + instance);
-        }
-    }
+		final Object previousObject = MAP.put(instance.getClass(), instance);
+		if (previousObject != null) {
+			throw new IllegalStateException("MAP already contains value for key=" + instance);
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    public static <T> T get(final Class<T> clazz) throws IllegalStateException {
-        final Object instance = MAP.get(clazz);
-        if (instance == null) {
-            throw new IllegalStateException("No instance of the class "
-                    + clazz.getName() + " was registered");
-        }
+	public static <T> void register(Class<T> clazz, final T instance) {
+		log.info("Registering '" + instance.getClass().getName() + "'");
 
-        return (T) instance;
-    }
+		final Object previousObject = MAP.put(clazz, instance);
+		if (previousObject != null) {
+			throw new IllegalStateException("MAP already contains value for key=" + instance);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T get(final Class<T> clazz) throws IllegalStateException {
+		final Object instance = MAP.get(clazz);
+		if (instance == null) {
+			throw new IllegalStateException("No instance of the class " + clazz.getName() + " was registered");
+		}
+
+		return (T) instance;
+	}
 }
