b996e72b901f36026fb8c84c7b00190a4e55d8d9
Adisesh <adisesh.viruru@gmail.com>
2018-01-24 17:29:02 -0600

    Fix RobotInfo

--- src/org/usfirst/frc/team2175/info/RobotInfo.java
+++ src/org/usfirst/frc/team2175/info/RobotInfo.java
@@ -19,7 +19,7 @@ public class RobotInfo {
 	public static final String RIGHT_MOTOR_SLAVE2 = "drivetrain.motor.right.slave2";
 	public static final String INTAKE_MOTOR = "intake.motor";
 	public static final String ELEVATOR_MOTOR = "elevator.motor1";
-	public static final String ELEVATOR_MOTOR = "elevator.motor2";
+	public static final String ELEVATOR_MOTOR2 = "elevator.motor2";
 	public static final String LEFT_JOYSTICK = "driverstation.joystick.left";
 	public static final String RIGHT_JOYSTICK = "driverstation.joystick.right";
 	public static final String GAMEPAD = "driverstation.gamepad";
