b4835846d80681546db30fbea16f3a6452579a01
Ben Visness <bvisness@gmail.com>
2018-01-26 20:10:20 -0600

    Fix test compile errors

--- test/team2175/control/BetterDriveTest.java
+++ test/team2175/control/BetterDriveTest.java
@@ -1,7 +1,7 @@
 package team2175.control;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.usfirst.frc.team2175.control.BetterDrive;
@@ -11,49 +11,49 @@ public class BetterDriveTest {
 	@Test
 	public void testBetterDrive_StraightBack() {
 		double[] speed = BetterDrive.getMotorValues(-1, 0);
-		assertEquals(speed[0], -1);
-		assertEquals(speed[1], -1);
+		assertEquals(-1, speed[0], 0);
+		assertEquals(-1, speed[1], 0);
 	}
 
 	@Test
 	public void testBetterDrive_StraightForward() {
 		double[] speed = BetterDrive.getMotorValues(1, 0);
-		assertEquals(speed[0], 1);
-		assertEquals(speed[1], 1);
+		assertEquals(1, speed[0], 0);
+		assertEquals(1, speed[1], 0);
 	}
 
 	@Test
 	public void testBetterDrive_CurvingRight() {
 		double[] speed = BetterDrive.getMotorValues(1, .5);
-		assertEquals(speed[0], 1);
-		assertEquals(speed[1], 0.333, 0.01);
+		assertEquals(1, speed[0], 0);
+		assertEquals(0.333, speed[1], 0.01);
 	}
 
 	@Test
 	public void testBetterDrive_CurvingLeft() {
 		double[] speed = BetterDrive.getMotorValues(1, -.5);
-		assertEquals(speed[0], 0.333, 0.01);
-		assertEquals(speed[1], 1);
+		assertEquals(0.333, speed[0], 0.01);
+		assertEquals(1, speed[1], 0);
 	}
 
 	@Test
 	public void testBetterDrive_CurvingRightSlowly() {
 		double[] speed = BetterDrive.getMotorValues(0.5, .5);
-		assertEquals(speed[0], 1);
-		assertEquals(speed[1], 0.333, 0.01);
+		assertEquals(1, speed[0], 0);
+		assertEquals(0.333, speed[1], 0.01);
 	}
 
 	@Test
 	public void testBetterDrive_CurvingLeftSlowly() {
 		double[] speed = BetterDrive.getMotorValues(0.5, -.5);
-		assertEquals(speed[0], 0.333, 0.01);
-		assertEquals(speed[1], 1);
+		assertEquals(0.333, speed[0], 0.01);
+		assertEquals(1, speed[1], 0);
 	}
 
 	@Test
 	public void testBetterDrive_JustTurning() {
 		double[] speed = BetterDrive.getMotorValues(0, -1);
-		assertTrue(speed[0] == 0);
-		assertTrue(speed[1] == 0);
+		assertEquals(0, speed[0], 0);
+		assertEquals(0, speed[1], 0);
 	}
 }
