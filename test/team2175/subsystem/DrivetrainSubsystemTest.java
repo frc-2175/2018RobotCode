a5688993a87283d6a246f88c1d49e825918b806f
Ben Visness <bvisness@gmail.com>
2018-01-28 16:01:48 -0600

    Add test for undeadband

--- test/team2175/subsystem/DrivetrainSubsystemTest.java
+++ test/team2175/subsystem/DrivetrainSubsystemTest.java
@@ -1,11 +1,12 @@
 package team2175.subsystem;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.Test;
 import org.usfirst.frc.team2175.subsystem.DrivetrainSubsystem;
 
 import edu.wpi.first.wpilibj.drive.RobotDriveBase;
-
-import static org.junit.Assert.assertArrayEquals;
 import team2175.BaseTest;
 
 public class DrivetrainSubsystemTest extends BaseTest {
@@ -17,22 +18,44 @@ public class DrivetrainSubsystemTest extends BaseTest {
 		final double[] blends = DrivetrainSubsystem.getBlendedMotorValues(moveValue, turnValue);
 		assertArrayEquals(expectedBlends, blends, 0.001);
 	}
-	
+
 	public static double undeadband(double value, double deadband) {
 		if (value < 0) {
 			double t = -value;
 			return DrivetrainSubsystem.lerp(-deadband, -1, t);
 		} else if (value > 0) {
 			double t = value;
-			return -DrivetrainSubsystem.lerp(deadband, 1, t);
+			return DrivetrainSubsystem.lerp(deadband, 1, t);
 		} else {
 			return 0;
 		}
 	}
-	
-//	@Test
-//	public void testGetBlendedMotorValues_Left_Zero_Right_Full() {
-//		final double moveValue = 0;
-//		final double turnValue = 
-//	}
+
+	@Test
+	public void testUndeadband() {
+		assertEquals(-1, undeadband(applyDeadband(-1, RobotDriveBase.kDefaultDeadband), RobotDriveBase.kDefaultDeadband), 0.00001);
+		assertEquals(-0.5, undeadband(applyDeadband(-0.5, RobotDriveBase.kDefaultDeadband), RobotDriveBase.kDefaultDeadband), 0.00001);
+		assertEquals(0, undeadband(applyDeadband(0, RobotDriveBase.kDefaultDeadband), RobotDriveBase.kDefaultDeadband), 0.00001);
+		assertEquals(0.5, undeadband(applyDeadband(0.5, RobotDriveBase.kDefaultDeadband), RobotDriveBase.kDefaultDeadband), 0.00001);
+		assertEquals(1, undeadband(applyDeadband(1, RobotDriveBase.kDefaultDeadband), RobotDriveBase.kDefaultDeadband), 0.00001);
+	}
+
+	/** Straight copy-paste from RobotDriveBase. */
+	protected double applyDeadband(double value, double deadband) {
+		if (Math.abs(value) > deadband) {
+			if (value > 0.0) {
+				return (value - deadband) / (1.0 - deadband);
+			} else {
+				return (value + deadband) / (1.0 - deadband);
+			}
+		} else {
+			return 0.0;
+		}
+	}
+
+	//	@Test
+	//	public void testGetBlendedMotorValues_Left_Zero_Right_Full() {
+	//		final double moveValue = 0;
+	//		final double turnValue = 
+	//	}
 }
