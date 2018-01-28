2069f023ce4dba008bc079d16a38bd462e142993
Ben Visness <bvisness@gmail.com>
2018-01-28 15:01:30 -0600

    Add undeadband method

--- test/team2175/subsystem/DrivetrainSubsystemTest.java
+++ test/team2175/subsystem/DrivetrainSubsystemTest.java
@@ -17,6 +17,18 @@ public class DrivetrainSubsystemTest extends BaseTest {
 		assertEquals(expectedBlends, blends);
 	}
 	
+	public static double undeadband(double value, double deadband) {
+		if (value < 0) {
+			double t = -value;
+			return DrivetrainSubsystem.lerp(-deadband, -1, t);
+		} else if (value > 1) {
+			double t = value;
+			return DrivetrainSubsystem.lerp(deadband, 1, t);
+		} else {
+			return 0;
+		}
+	}
+	
 //	@Test
 //	public void testGetBlendedMotorValues_Left_Zero_Right_Full() {
 //		final double moveValue = 0;
