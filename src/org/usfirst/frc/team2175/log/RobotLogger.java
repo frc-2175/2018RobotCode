9fd88b878881f5feb359bd6c329a33ab73bc4dd1
Kevin La Casse <kl.forthwind@gmail.com>
2018-02-01 18:38:23 -0600

    Change working directory for log files

--- src/org/usfirst/frc/team2175/log/RobotLogger.java
+++ src/org/usfirst/frc/team2175/log/RobotLogger.java
@@ -9,7 +9,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.usfirst.frc.team2175.ServiceLocator;
-import org.usfirst.frc.team2175.robot.Robot;
 
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
@@ -20,53 +19,53 @@ import edu.wpi.first.wpilibj.Timer;
 public class RobotLogger {
 	private ArrayList<Loggable> loggers;
 	private final static Logger log = Logger.getLogger(RobotLogger.class.getName());
-	
+	public final static String BASE_DIRECTORY = "/home/lvuser/log/";
+
 	public static class LogEntry {
 		double timestamp;
 		HashMap<String, Object> values;
-		
+
 		public LogEntry(HashMap<String, Object> values) {
 			this.timestamp = Timer.getFPGATimestamp();
 			this.values = values;
 		}
 	}
-	
+
 	private Gson gson;
 	private HashMap<Loggable, BufferedWriter> writers;
-	
+
 	public RobotLogger() {
-		gson = new GsonBuilder()
-				.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-//				.setDateFormat("yyyy-MM-ddTHH:mm:ss.SSSX")
+		gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+				// .setDateFormat("yyyy-MM-ddTHH:mm:ss.SSSX")
 				.create();
 		writers = new HashMap<>();
-		
+
 		ServiceLocator.register(this);
-		
+
 		loggers = new ArrayList<>();
 		loggers.add(new TestLoggable());
 	}
-	
+
 	public static String getLogFilename(Loggable l) {
-		return l.getLogType() + "-" + l.getId() + ".log";
+		return BASE_DIRECTORY + l.getLogType() + "-" + l.getId() + ".log";
 	}
-	
+
 	public void logLoggable(Loggable l) throws IOException {
 		BufferedWriter w = writers.get(l);
 		if (w == null) {
 			w = new BufferedWriter(new FileWriter(getLogFilename(l)));
 			writers.put(l, w);
 		}
-		
+
 		String json = gson.toJson(new LogEntry(l.getValues()));
 		w.write(json + "\n");
 	}
-	
+
 	public void log() {
-		for(Loggable logger : loggers) {
-			try { 
-				logLoggable(logger); 
-			} catch(IOException e) {
+		for (Loggable logger : loggers) {
+			try {
+				logLoggable(logger);
+			} catch (IOException e) {
 				log.log(Level.SEVERE, "Failed to log " + getLogFilename(logger), e);
 			}
 		}
