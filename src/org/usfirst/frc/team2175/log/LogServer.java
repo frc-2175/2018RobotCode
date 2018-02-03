2ffa08c6267ad77ea124bf3f3074207da55ea10e
Noah Connors <noahconnors@mnmsa.org>
2018-02-03 13:48:40 -0600

    Return the json representation of the directory contents

--- src/org/usfirst/frc/team2175/log/LogServer.java
+++ src/org/usfirst/frc/team2175/log/LogServer.java
@@ -1,4 +1,9 @@
 package org.usfirst.frc.team2175.log;
+import java.io.File;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
 import io.javalin.Javalin;
 
 public class LogServer {
@@ -7,7 +12,19 @@ public class LogServer {
 		@Override
 		public void run() {
 			Javalin app = Javalin.start(7000);
-	        app.get("/", ctx -> ctx.result("Hello World"));
+			Gson gson = new Gson();
+	        app.get("/", ctx -> {
+	        	File baseDirectory = new File(RobotLogger.BASE_DIRECTORY);
+	        	ArrayList<File> directoryContents = new ArrayList<>();
+	        	for(File file : baseDirectory.listFiles()) {
+	        		if(file.isDirectory()) {
+	        			directoryContents.add(file);
+	        		}
+	        	}
+	        	String json = gson.toJson(directoryContents);
+	        	ctx.header("Content-Type", "application/json");
+	        	ctx.result(json);
+	        });
 	        System.out.println("The server is started!");
 		}
 	}
