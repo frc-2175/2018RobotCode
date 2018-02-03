41cd8de531caacbe6098b63f91aa66a142f8b992
Erik Nelson <eriknelson@mnmsa.org>
2018-02-02 20:32:47 -0600

    Put stop methods in SetElevatorToBottomPositonCommand, SetElevatorToTopPositionCommand, and SpinIntakeOutCommand

--- src/org/usfirst/frc/team2175/command/autonomous/SetElevatorToBottomPositionCommand.java
+++ src/org/usfirst/frc/team2175/command/autonomous/SetElevatorToBottomPositionCommand.java
@@ -1,19 +1,26 @@
 package org.usfirst.frc.team2175.command.autonomous;
 
 import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.single.BaseCommand;
 import org.usfirst.frc.team2175.subsystem.ElevatorSubsystem;
 
-public class SetElevatorToBottomPositionCommand {
+public class SetElevatorToBottomPositionCommand extends BaseCommand {
 	private ElevatorSubsystem elevatorSubsystem;
 
 	public SetElevatorToBottomPositionCommand() {
 		elevatorSubsystem = ServiceLocator.get(ElevatorSubsystem.class);
 		
 	}
+	@Override
 	protected void execute() {
 		elevatorSubsystem.runElevatorDown();
 	}
+	@Override
 	protected boolean isFinished() {
 		return false;
+	}
+	@Override
+	protected void end() {
+		elevatorSubsystem.stopElevator();
 	}
 }
--- src/org/usfirst/frc/team2175/command/autonomous/SpinIntakeOutCommand.java
+++ src/org/usfirst/frc/team2175/command/autonomous/SpinIntakeOutCommand.java
@@ -1,18 +1,25 @@
 package org.usfirst.frc.team2175.command.autonomous;
 
 import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.single.BaseCommand;
 import org.usfirst.frc.team2175.subsystem.IntakeSubsystem;
 
-public class SpinIntakeOutCommand {
+public class SpinIntakeOutCommand extends BaseCommand {
 	private IntakeSubsystem intakeSubsystem;
 	
 	public SpinIntakeOutCommand() {
 		intakeSubsystem = ServiceLocator.get(IntakeSubsystem.class);
 	}
+	@Override
 	protected void execute() {
 		intakeSubsystem.runIntakeOut();
 	}
+	@Override
 	protected boolean isFinished() {
 		return false;
+	}
+	@Override
+	protected void end() {
+		intakeSubsystem.stopAllMotors();
 	}
 }
