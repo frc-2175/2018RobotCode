ce46f1e0a4e8dce2c6a4d57c4f67eb432d0ae788
Nupur Kumar <kumarnupurk@gmail.com>
2018-02-02 20:29:46 -0600

    Add LowerIntakeCommand, RaiseIntakeFullCommand, and RaiseIntakeHalfCommand to auto

---
+++ src/org/usfirst/frc/team2175/command/autonomous/RaiseIntakeHalfCommand.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team2175.command.autonomous;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.single.BaseCommand;
+import org.usfirst.frc.team2175.subsystem.IntakeSubsystem;
+
+public class RaiseIntakeHalfCommand extends BaseCommand {
+	private IntakeSubsystem intakeSubsystem;
+		
+	public RaiseIntakeHalfCommand() {
+		intakeSubsystem = ServiceLocator.get(IntakeSubsystem.class);
+	}
+		
+	@Override 
+	protected void initialize() {
+		intakeSubsystem.moveHalf();
+	}
+		
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+}
