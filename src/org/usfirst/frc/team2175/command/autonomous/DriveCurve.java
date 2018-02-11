fa9cb0566db25ec2221bbf860fee4cdebddc0672
Kevin La Casse <kl.forthwind@gmail.com>
2018-02-11 17:24:35 -0600

    Create DriveCurve for autonomous

---
+++ src/org/usfirst/frc/team2175/command/autonomous/DriveCurve.java
@@ -0,0 +1,51 @@
+package org.usfirst.frc.team2175.command.autonomous;
+
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.single.BaseCommand;
+import org.usfirst.frc.team2175.subsystem.DrivetrainSubsystem;
+
+public class DriveCurve extends BaseCommand {
+	private DrivetrainSubsystem drivetrainSubsystem;
+	private double radians;
+	private double leftSpeed;
+	private double rightSpeed;
+	private double widthOfBot = 26.125;
+
+	public DriveCurve(double radius, double degrees, double maxSpeed) {
+		drivetrainSubsystem = ServiceLocator.get(DrivetrainSubsystem.class);
+		radians = Math.toRadians(degrees);
+		double r2 = (2 * radius - widthOfBot);
+		double r1 = (2 * radius + widthOfBot);
+		boolean radiusPositive = radius > 0;
+		if (radiusPositive) {
+			double wheelRatio = r2 / r1;
+			leftSpeed = maxSpeed;
+			rightSpeed = wheelRatio * maxSpeed;
+		} else {
+			double wheelRatio = r1 / r2;
+			leftSpeed = wheelRatio * maxSpeed;
+			rightSpeed = maxSpeed;
+		}
+	}
+
+	@Override
+	public void initialize() {
+		super.initialize();
+		drivetrainSubsystem.resetAllSensors();
+	}
+
+	@Override
+	public void execute() {
+		if (radians > 0) {
+			drivetrainSubsystem.autonDrive(leftSpeed, rightSpeed);
+		} else {
+			drivetrainSubsystem.autonDrive(-leftSpeed, -rightSpeed);
+		}
+	}
+
+	@Override
+	public boolean isFinished() {
+		double targetDiff = Math.toRadians(Math.abs(drivetrainSubsystem.getGyroValue())) - Math.abs(radians);
+		return targetDiff > 0;
+	}
+}
