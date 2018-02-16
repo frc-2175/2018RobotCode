c9fd715c1a0d0798d380cf1ca7c1e45241c025b6
Ben Visness <bvisness@gmail.com>
2018-02-15 19:37:07 -0600

    Add LambdaConditionalCommand

---
+++ src/org/usfirst/frc/team2175/command/autonomous/LambdaConditionalCommand.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team2175.command.autonomous;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.ConditionalCommand;
+
+public class LambdaConditionalCommand extends ConditionalCommand {
+
+	public static interface Condition {
+		public boolean check();
+	}
+
+	protected Condition condition;
+
+	public LambdaConditionalCommand(Condition condition, Command onTrue) {
+		super(onTrue);
+		this.condition = condition;
+	}
+
+	public LambdaConditionalCommand(Condition condition, Command onTrue, Command onFalse) {
+		super(onTrue, onFalse);
+		this.condition = condition;
+	}
+
+	@Override
+	protected boolean condition() {
+		return condition.check();
+	}
+
+}
