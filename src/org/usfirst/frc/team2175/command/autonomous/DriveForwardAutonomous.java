d1647372c68e259cccf5268c742444553de14fb0
Kevin La Casse <kl.forthwind@gmail.com>
2018-02-15 20:04:15 -0600

    make lambda conditional command

---
+++ src/org/usfirst/frc/team2175/command/autonomous/DriveForwardAutonomous.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team2175.command.autonomous;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveForwardAutonomous extends Command {
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
