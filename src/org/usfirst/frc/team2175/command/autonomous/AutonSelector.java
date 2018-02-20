03637e9da39d7690f1317616a38b5782bd12ee44
Kevin La Casse <kl.forthwind@gmail.com>
2018-02-19 18:22:22 -0600

    Change Up Auton Selector

--- src/org/usfirst/frc/team2175/command/autonomous/AutonSelector.java
+++ src/org/usfirst/frc/team2175/command/autonomous/AutonSelector.java
@@ -1,21 +1,107 @@
 package org.usfirst.frc.team2175.command.autonomous;
 
+import org.usfirst.frc.team2175.ServiceLocator;
+import org.usfirst.frc.team2175.command.LambdaConditionalCommand;
+import org.usfirst.frc.team2175.control.DryverStation;
+
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutonSelector {
-	private SendableChooser<Command> m_chooser = new SendableChooser<>();
+	private DryverStation driverStation;
+
+	private SendableChooser<Side> side = new SendableChooser<>();
+	private SendableChooser<Command> switchAndScale = new SendableChooser<>();
+	private SendableChooser<Command> justSwitch = new SendableChooser<>();
+	private SendableChooser<Command> justScale = new SendableChooser<>();
+	private SendableChooser<Command> neither = new SendableChooser<>();
+	private SendableChooser<Command> temp = new SendableChooser<>();
+	private SendableChooser<Command> center = new SendableChooser<>();
 
 	public AutonSelector() {
-		m_chooser.addObject("kurveRight", new KurveDriveRightSideOfSwitch());
-		m_chooser.addObject("DriveCurve", new DriveCurveAutonomous());
-		m_chooser.addObject("Dumb Drive", new CrossBaselineTimeBasedAutonomous());
-		m_chooser.addDefault("Do Nothing", new DoNothingCommandGroup());
-		SmartDashboard.putData("Auto choices", m_chooser);
+		driverStation = ServiceLocator.get(DryverStation.class);
+		addSides();
+		addSwitchAndScale();
+		addJustSwitch();
+		addJustScale();
+		addNeither();
+		addCenter();
+		addTemp();
+	}
+
+	private void addTemp() {
+		temp.addDefault("Do Nothing", new DoNothingCommandGroup());
+		temp.addObject("kurveRight", new KurveDriveRightSideOfSwitch());
+		temp.addObject("DriveCurve", new DriveCurveAutonomous());
+		temp.addObject("Dumb Drive", new CrossBaselineTimeBasedAutonomous());
+		SmartDashboard.putData("Temporary", temp);
+	}
+
+	private void addSwitchAndScale() {
+		switchAndScale.addDefault("Do Nothing", new DoNothingCommandGroup());
+		SmartDashboard.putData("SwitchAndScale", switchAndScale);
+	}
+
+	private void addJustSwitch() {
+		justSwitch.addDefault("Do Nothing", new DoNothingCommandGroup());
+		SmartDashboard.putData("JustSwitch", justSwitch);
+
+	}
+
+	private void addJustScale() {
+		justScale.addDefault("Do Nothing", new DoNothingCommandGroup());
+		SmartDashboard.putData("JustScale", justScale);
+	}
+
+	private void addNeither() {
+		neither.addDefault("Do Nothing", new DoNothingCommandGroup());
+		SmartDashboard.putData("Neither", neither);
+	}
+
+	private void addCenter() {
+		center.addDefault("Do Nothing", new DoNothingCommandGroup());
+		SmartDashboard.putData("Center", center);
+	}
+
+	private void addSides() {
+		for (Side s : Side.values()) {
+			side.addObject(s.toString(), s);
+		}
+		SmartDashboard.putData("SideChoice", side);
 	}
 
 	public Command getCommand() {
-		return m_chooser.getSelected();
+		Command selectedCommand;
+		switch (side.getSelected()) {
+		case Left:
+			selectedCommand = new LambdaConditionalCommand(() -> driverStation.isSwitchLeft(),
+				new LambdaConditionalCommand(() -> driverStation.isScaleLeft(), switchAndScale.getSelected(),
+					justSwitch.getSelected()),
+				new LambdaConditionalCommand(() -> driverStation.isScaleLeft(), justScale.getSelected(),
+					neither.getSelected()));
+			break;
+		case Right:
+			selectedCommand = new LambdaConditionalCommand(() -> !driverStation.isSwitchLeft(),
+				new LambdaConditionalCommand(() -> !driverStation.isScaleLeft(), switchAndScale.getSelected(),
+					justSwitch.getSelected()),
+				new LambdaConditionalCommand(() -> !driverStation.isScaleLeft(), justScale.getSelected(),
+					neither.getSelected()));
+			break;
+		case Center:
+			selectedCommand = center.getSelected();
+			break;
+		case Test:
+			selectedCommand = temp.getSelected();
+			break;
+		default:
+			selectedCommand = new DoNothingCommandGroup();
+			break;
+		}
+		return selectedCommand;
+	}
+
+	enum Side {
+		Left, Right, Center, Test;
 	}
 }
